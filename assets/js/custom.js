6704b5f5af0bb52eedf828472ca64917c781a1a0
Bilal916885 <153007707+Bilal916885@users.noreply.github.com>
2023-12-26 10:16:55 -0800

    ok
    dd

---
+++ assets/js/custom.js
@@ -0,0 +1,37 @@
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:false,
+//     autoplay:true,
+//     autoplayTimeout:1000,
+//     autoplayHoverPause:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:1
+//         },
+//         1000:{
+//             items:1
+//         }
+//     }
+// })
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
